934265103913566c5366a116fe820968e61ce942
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-11-26 16:55:26 -0500

    Update IronMonke.cs

--- IronMonke.cs
+++ IronMonke.cs
@@ -5,12 +5,15 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 public class IronMonke : MonoBehaviour
 {
+//apply script to contoller gameobject
             private XRController controller = null;
         public GameObject Player;
         public Transform Hand;
         public Rigidbody PlayerRB;
+        //player camera (unused)
         public Transform Cam;
         public float speed = 2f;
+        //toggle this to true if left hand because the left hand has inverted values 
         public bool leftHand = false;
     // Start is called before the first frame update
     void Start()
